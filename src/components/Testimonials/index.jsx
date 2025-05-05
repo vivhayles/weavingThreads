a44426d4f3b79903cc59b618cac37e0511895daa
Viv Hayles <vivhayles@gmail.com>
2025-05-05 15:52:31 +0100

    Created testimonial component and added logo

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -1,8 +1,8 @@
-import codingprojects from "../../../codingprojects.json";
+import testimonials from "../../../testimonials.json";
 import { useState } from "react";
-import Heading from "../Heading";
 
-function Portfolio() {
+
+function Testimonials() {
     const [openModalIndex, setOpenModalIndex] = useState(null);
 
     function handleClick(index) {
@@ -10,40 +10,23 @@ function Portfolio() {
     }
 
     return (
-        <section id="portfolio-of-work">
-            <Heading text='Portfolio'/>
-            <p className="mx-4 lg:text-lg">Here is a showcase of my coding projects so far:</p>
+        <section id="testimonials">
+            <p className="mx-4 lg:text-lg">Here are a selection of testimonials from past and present clients:</p>
             <div className="grid grid-cols-1 md:grid md:grid-cols-2 gap-1">
-                {codingprojects.project.map((project, index) => (
-                    <div key={index} className="project-card mx-4 my-2 border-4
+                {testimonials.testimonials.map((testimonial, index) => (
+                    <div key={index} className="testimonial-card mx-4 my-2 border-4
                     border-[#8eb8a8] rounded-lg p-2">
-                        <h4 className="py-2 font-bold text-xl text-[#737373]">{project.name}</h4>
-                        <p className='lg:text-lg'>{project.description}</p>
-                        <img
-                            src={project.img}
-                            alt={`${project.name} screenshot`}
-                            className="project-img py-2"
-                        />
-                        <div className="lg:text-lg links grid grid-cols-2 gap-1 text-center
-                        text-gray-600 font-semibold underline">
-                            <a href={project.githublink} target="_blank">
-                                GitHub
-                            </a>
-                            {project.livelink && (
-                                <a href={project.livelink} target="_blank">
-                                    Live link
-                                </a>
-                            )}
-                        </div>
+                        <h4 className="py-2 font-bold text-xl text-[#737373]">{testimonial.title}</h4>
+                        <p className='lg:text-lg'>{testimonial.description}</p>
                         <button
                             className="px-2 my-2 border-4 border-[#cae4da] bg-[#cae4da] text-gray-600
                             font-semibold text-lg rounded-full flex justify-center mx-auto"
                             onClick={() => handleClick(index)}>
-                            {openModalIndex === index ? "Less" : "More"} {project.modalbutton}
+                            {openModalIndex === index ? "Less" : "More"} {testimonial.modalbutton}
                         </button>
                         {openModalIndex === index && (
-                            <div className="modal-content lg:text-lg mt-4 p-4 border rounded bg-[#f6f4ef]">
-                                <p>{project.moreinfo}</p>
+                            <div className="modal-content lg:text-lg mt-4 p-4 border rounded">
+                                <p>{testimonial.moreinfo}</p>
                             </div>
                         )}
                     </div>
@@ -53,4 +36,4 @@ function Portfolio() {
     );
 }
 
-export default Portfolio;
+export default Testimonials;
--- src/pages/TestimonialsPage/index.jsx
+++ src/pages/TestimonialsPage/index.jsx
@@ -1,9 +1,11 @@
 import Footer from "../../components/Footer/index.jsx";
+import Testimonials from "../../components/Testimonials/index.jsx";
 
 function TestimonialsPage() {
     return (
         <section className='bg-[#efecdf] font-serif'>
             <h1 className="mx-4 my-1 text-2xl md:text-3xl font-serif text-[#00472d] justify-center">Testimonials</h1>
+            <Testimonials />
             <Footer />
         </section>
     )
