38376f043d38e297adc25d63d0862d9f12f95058
Viv Hayles <vivhayles@gmail.com>
2025-05-05 12:43:28 +0100

    Added about me text

--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -7,7 +7,26 @@ function Aboutme() {
                  alt="A picture of Ruth a smiling white woman with short brown hair" src={pictureofme}/>
             <Subheading text="About Me"/>
             <div className="lg:text-lg m-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut metus mauris, lobortis sed dapibus volutpat, lacinia eget orci. Donec venenatis libero ipsum, et scelerisque odio mollis commodo. Maecenas lacinia pellentesque purus vitae sagittis. In ipsum lorem, molestie vitae pellentesque non, malesuada in quam. Vestibulum a arcu quis lectus faucibus bibendum. Donec id tortor maximus, facilisis nulla ac, fringilla erat. Phasellus vitae lectus ex. Aliquam congue ipsum id libero lacinia efficitur. Donec faucibus sem eget nisl venenatis, nec lacinia nunc faucibus. Duis vestibulum nibh tempor ante consequat mollis et nec est. Vivamus pellentesque purus vitae efficitur condimentum. Sed viverra ut augue at consectetur.
+                My journey to becoming an Emotion/Body/Belief Code Practitioner started long before I knew what I was
+                doing. Like many healers, I was living life as best I could, but something always felt off. I wasn’t
+                being true to myself, and I often buried the hard things that were too difficult to face.
+                <br/>
+                <br/>
+                When recurring health issues like Candida started to affect me, I realized it was time for a change.
+                This led me to discover energy healing, with a nudge from Louise Hay’s book You Can Heal Your Life. Over
+                the next few years, I kept coming back to it, but it wasn’t until 2012 that I found my real
+                breakthrough.
+                <br/>
+                <br/>
+                That’s when I came across Dr. Bradley Nelson’s Emotion Code and knew I had found something
+                life-changing. The Emotion Code helped me understand how our subconscious holds onto trapped emotions,
+                creating imbalances that can affect our health and wellbeing. As I worked with practitioners to release
+                these imbalances, I felt better—physically and emotionally—sometimes immediately, sometimes over time.
+                <br/>
+                <br/>
+                Now, as a trained Emotion, Body, and Belief Code Practitioner, I’m passionate about helping others
+                release their trapped emotions and find balance, healing, and freedom from what’s been holding them
+                back.
             </div>
         </section>
     )
